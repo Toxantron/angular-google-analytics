061e92f05cdd45dd37119f02ce2d99a65d165158
Toxantron <thomas.fuchs91@gmail.com>
2016-06-03 08:14:47 +0200

    More fluent API, decorator config

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
 
+// Class that represents a page hit
 class PageView {
-  constructor(public page : string, public title : string) { }
+  constructor(public page : string, // Page URL
+              public title : string, // Title of the page
+              ) {
+  }
 }
 
 // Interface for universal and classic analytics
@@ -37,32 +41,32 @@ class UniversalTracker implements ITracker {
 @Injectable()
 export class AnalyticsConfig {
   // All config values of the analytics module
-  private _accounts : any[];
-  private _created : boolean = false;
-  private _crossDomainLinker : boolean = false;
-  private _crossLinkDomains : boolean = false;
-  private _currency : string = 'USD';
-  private _debugMode : boolean = false;
-  private _delayScriptTag : boolean = false;
-  private _displayFeatures : boolean = false;
-  private _disableAnalytics : boolean = false;
-  private _domainName : string;
-  private _ecommerce : boolean = false;
-  private _enhancedEcommerce : boolean = false;
-  private _enhancedLinkAttribution : boolean = false;
-  private _experimentId : string;
-  private _ignoreFirstPageLoad : boolean = false;
-  private _logAllCalls : boolean = false;
-  private _hybridMobileSupport : boolean = false;
-  private _offlineMode : boolean = false;
-  private _pageEvent : string = '$routeChangeSuccess';
-  private _readFromRoute : boolean = false;
-  private _removeRegExp : string;
-  private _testMode : boolean = false;
-  private _traceDebuggingMode : boolean = false;
-  private _trackPrefix : string = '';
-  private _trackRoutes : boolean = true;
-  private _trackUrlParams : boolean = false;
+  public _accounts : any[];
+  public _created : boolean = false;
+  public _crossDomainLinker : boolean = false;
+  public _crossLinkDomains : boolean = false;
+  public _currency : string = 'USD';
+  public _debugMode : boolean = false;
+  public _delayScriptTag : boolean = false;
+  public _displayFeatures : boolean = false;
+  public _disableAnalytics : boolean = false;
+  public _domainName : string;
+  public _ecommerce : boolean = false;
+  public _enhancedEcommerce : boolean = false;
+  public _enhancedLinkAttribution : boolean = false;
+  public _experimentId : string;
+  public _ignoreFirstPageLoad : boolean = false;
+  public _logAllCalls : boolean = false;
+  public _hybridMobileSupport : boolean = false;
+  public _offlineMode : boolean = false;
+  public _pageEvent : string = '$routeChangeSuccess';
+  public _readFromRoute : boolean = false;
+  public _removeRegExp : string;
+  public _testMode : boolean = false;
+  public _traceDebuggingMode : boolean = false;
+  public _trackPrefix : string = '';
+  public _trackRoutes : boolean = true;
+  public _trackUrlParams : boolean = false;
   
   private _tracker : ITracker = new ClassicTracker();
   
@@ -78,38 +82,77 @@ export class AnalyticsConfig {
     return this;
   }
   
-  setDomainName(domain : string) {
+  setDomainName(domain : string) : AnalyticsConfig {
     this._domainName = domain;
     return this;
   }
   
-  useDisplayFeatures(val : boolean) {
+  useDisplayFeatures(val : boolean) : AnalyticsConfig {
     this._displayFeatures = val;
     return this;
   }
   
-  useAnalytics(val : boolean) {
+  useAnalytics(val : boolean) : AnalyticsConfig {
     this._tracker = val ? new UniversalTracker() : new ClassicTracker();
     return this;
   }
   
-  useEnhancedLinkAttribution(val : boolean) {
+  useEnhancedLinkAttribution(val : boolean) : AnalyticsConfig {
     this._enhancedLinkAttribution = val;
     return this;
   }
   
-  useCrossDomainLinker(val : boolean) {
+  useCrossDomainLinker(val : boolean) : AnalyticsConfig {
     this._crossDomainLinker = val;
     return this;
   }
   
-  setCrossLinkDomains(domains) {
+  setCrossLinkDomains(domains : boolean) : AnalyticsConfig {
     this._crossLinkDomains = domains;
     return this;
   }
   
+  setPageEvent(name : string) : AnalyticsConfig {
+    this_pageEvent = name;
+    return this;
+  }
+  
+  useECommerce(val : boolean, enhanced : boolean) : AnalyticsConfig {
+    this._ecommerce = val;
+    this._enhancedEcommerce = enhanced;
+    return this;
+  }
+  
+  setCurrency(currencyCode : string) : AnalyticsConfig {
+    this._currency = currencyCode;
+    return this;
+  }
+  
+  setRemoveRegExp(regex : RegExp) : AnalyticsConfig {
+    
+    removeRegExp = regex;
+    return this;
+  }
   
   // <---- End of fluent API
+}
+
+/*
+ * Decorator for the decorator approach
+ * @Tracking({
+ *   readFromRoute : true,
+ *   useECommerce : true
+ * })
+ * class AppComponent { }
+ */
+export Tracking(target) {
+  return function(config) {
+    var analyticsConfig = new AnalyticsConfig();
+    for (var key in config) {
+      analyticsConfig['_' + key] = config[key];
+    }
+    target.$analytics = analyticsConfig;
+  }
 }
 
 @Injectable()
